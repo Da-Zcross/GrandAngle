4b34e2479c1fce5f9bc203e174fba1050e563396
mounzor <jb.mounier@yahoo.com>
2024-03-16 17:59:29 +0100

    delete ok, pagination ok

--- admin/artistes.php
+++ admin/artistes.php
@@ -37,138 +37,128 @@ include "includes/pages/header.php";
           </thead>
           <tbody>
             <?php foreach ($artistes as $artiste) : ?>
-                <div class="project-box-wrapper" id="<?= $artiste["id_artiste"] ?>">
-                  <div class="delete_artiste_overlay" id="delete_artiste_overlay-<?= $artiste['id_artiste'] ?>">
-                    <div class="container_dlt">
-                      <div class="infos_dlt">
-                        <p>Voulez-vous vraiment supprimer </span><span id="artiste_nom"></span> <span id="artiste_prenom"></span></p>
-                        <div class="button_dlt">
-                          <button id="confirm_delete_button" data-artiste-id="<?= $artiste['id_artiste'] ?>">Oui, supprimer maintenant</button>
-                          <button id="cancel_delete_button">Non, supprimer plus tard</button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <tr>
-                    <td><?= $artiste["id_artiste"] ?></td>
-                    <td><?= $artiste["nom_artiste"] ?></td>
-                    <td><?= $artiste["prenom_artiste"] ?></td>
-                    <td><?= $artiste["email_artiste"] ?></td>
-                    <td><?= $artiste["date_naissance_artiste"] ?></td>
-                    <td><?= $artiste["date_deces_artiste"] ?></td>
-                    <td><a href="updating_artiste.php?id_artiste=<?= $artiste['id_artiste'] ?>"><i class="fa-solid fa-pen"></i></a></td>
-                    <td><a href="#" class="delete_artiste_link link" data-id="<?= $artiste["id_artiste"] ?>" data-nom="<?= $artiste["nom_artiste"] ?>" data-prenom="<?= $artiste["prenom_artiste"] ?>"><i class="fa-solid fa-trash-can"></i></a></td>
-                  </tr>
+              <tr class="art_wrap" id="<?= $artiste["id_artiste"] ?>">
+                <td><?= $artiste["id_artiste"] ?></td>
+                <td><?= $artiste["nom_artiste"] ?></td>
+                <td><?= $artiste["prenom_artiste"] ?></td>
+                <td><?= $artiste["email_artiste"] ?></td>
+                <td><?= $artiste["date_naissance_artiste"] ?></td>
+                <td><?= $artiste["date_deces_artiste"] ?></td>
+                <td><a href="updating_artiste.php?id_artiste=<?= $artiste['id_artiste'] ?>"><i class="fa-solid fa-pen"></i></a></td>
+                <td><a href="#" class="delete_artiste_link link" data-id="<?= $artiste["id_artiste"] ?>" data-nom="<?= $artiste["nom_artiste"] ?>" data-prenom="<?= $artiste["prenom_artiste"] ?>"><i class="fa-solid fa-trash-can"></i></a></td>
+              </tr>
+              <div class="delete_artiste_overlay" id="delete_artiste_overlay-<?= $artiste['id_artiste'] ?>">
+                <p>Voulez-vous vraiment supprimer <?= $artiste['nom_artiste'] ?> <?= $artiste['prenom_artiste'] ?>?</p>
+                <div class="button_dlt">
+                  <button id="confirm_delete_button" data-artiste-id="<?= $artiste['id_artiste'] ?>">Oui, supprimer maintenant</button>
+                  <button id="cancel_delete_button">Non, supprimer plus tard</button>
                 </div>
-            <?php endforeach; ?>
-          </tbody>
-        </table>
+              </div>
       </div>
+    <?php endforeach; ?>
+    </tbody>
+    </table>
     </div>
+  </div>
   </div>
 </section>
 
+
 <script>
-  document.addEventListener('DOMContentLoaded', function() {
-    const content = document.getElementById('data');
-    const itemsPerPage = 5;
-    let currentPage = 0;
+document.addEventListener('DOMContentLoaded', function() {
+  const content = document.getElementById('data');
+  const itemsPerPage = 5;
+  let currentPage = localStorage.getItem('currentPage') || 0;
+
+  function showPage(page) {
     const items = Array.from(content.getElementsByTagName('tr')).slice(1);
+    const startIndex = page * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    items.forEach((item, index) => {
+      item.classList.toggle('hidden', index < startIndex || index >= endIndex);
+    });
+    updateActiveButtonStates();
+  }
 
-    function showPage(page) {
-      const startIndex = page * itemsPerPage;
-      const endIndex = startIndex + itemsPerPage;
-      items.forEach((item, index) => {
-        item.classList.toggle('hidden', index < startIndex || index >= endIndex);
+  function createPageButtons() {
+    const items = Array.from(content.getElementsByTagName('tr')).slice(1);
+    const totalPages = Math.ceil(items.length / itemsPerPage);
+    const paginationContainer = document.createElement('div');
+    const paginationDiv = document.body.appendChild(paginationContainer);
+    paginationContainer.classList.add('pagination');
+
+    for (let i = 0; i < totalPages; i++) {
+      const pageButton = document.createElement('button');
+      pageButton.textContent = i + 1;
+      pageButton.addEventListener('click', () => {
+        currentPage = i;
+        localStorage.setItem('currentPage', currentPage);
+        showPage(currentPage);
+        updateActiveButtonStates();
       });
-      updateActiveButtonStates();
+
+      content.appendChild(paginationContainer);
+      paginationDiv.appendChild(pageButton);
     }
+  }
 
-    function createPageButtons() {
-      const totalPages = Math.ceil(items.length / itemsPerPage);
-      const paginationContainer = document.createElement('div');
-      const paginationDiv = document.body.appendChild(paginationContainer);
-      paginationContainer.classList.add('pagination');
-
-      for (let i = 0; i < totalPages; i++) {
-        const pageButton = document.createElement('button');
-        pageButton.textContent = i + 1;
-        pageButton.addEventListener('click', () => {
-          currentPage = i;
-          showPage(currentPage);
-          updateActiveButtonStates();
-        });
-
-        content.appendChild(paginationContainer);
-        paginationDiv.appendChild(pageButton);
+  function updateActiveButtonStates() {
+    const pageButtons = document.querySelectorAll('.pagination button');
+    pageButtons.forEach((button, index) => {
+      if (index === currentPage) {
+        button.classList.add('active');
+      } else {
+        button.classList.remove('active');
       }
-    }
+    });
+  }
 
-    function updateActiveButtonStates() {
-      const pageButtons = document.querySelectorAll('.pagination button');
-      pageButtons.forEach((button, index) => {
-        if (index === currentPage) {
-          button.classList.add('active');
-        } else {
-          button.classList.remove('active');
-        }
-      });
-    }
-
-    createPageButtons();
-    showPage(currentPage);
-  });
+  createPageButtons();
+  showPage(currentPage);
 
   const deleteLinks = document.querySelectorAll('.delete_artiste_link');
 
   deleteLinks.forEach(function(deleteLink) {
-    const projectWrapper = deleteLink.closest('.project-box-wrapper')
-    const modal = deleteLink.closest('.bloc_list').querySelector('.delete_artiste_overlay');
-    const confirmButton = modal.querySelector('#confirm_delete_button');
-    const cancelButton = modal.querySelector('#cancel_delete_button');
-
     deleteLink.addEventListener('click', function(event) {
       event.preventDefault();
       const artisteId = this.getAttribute('data-id');
       const artisteNom = this.getAttribute('data-nom');
       const artistePrenom = this.getAttribute('data-prenom');
 
-      console.log('Lien cliqué, ID:', artisteId);
-      console.log('ID de l\'artiste à supprimer:', artisteId);
-      const modal = document.querySelector('.delete_artiste_overlay');
-      const artisteNomSpan = modal.querySelector('#artiste_nom');
-      const artistePrenomSpan = modal.querySelector('#artiste_prenom');
-      artisteNomSpan.textContent = artisteNom;
-      artistePrenomSpan.textContent = artistePrenom;
-      modal.style.display = "block";
-      confirmButton.setAttribute('data-artiste-id', artisteId);
-    });
+      const modalId = `delete_artiste_overlay-${artisteId}`;
+      const modal = document.getElementById(modalId);
 
+      if (modal) {
+        const confirmButton = modal.querySelector('#confirm_delete_button');
+        const cancelButton = modal.querySelector('#cancel_delete_button');
+        modal.style.display = "block";
+        confirmButton.setAttribute('data-artiste-id', artisteId);
 
-    cancelButton.addEventListener('click', function(event) {
-      event.preventDefault();
-      modal.style.display = "none";
-    });
+        cancelButton.addEventListener('click', function(event) {
+          event.preventDefault();
+          modal.style.display = "none";
+        });
 
-    confirmButton.addEventListener('click', function(event) {
-      event.preventDefault();
-      const artisteId = this.getAttribute('data-artiste-id');
-
-      const xhr = new XMLHttpRequest();
-      xhr.open('POST', 'delete_artiste.php');
-      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      xhr.onload = function() {
-        if (xhr.status === 200) {
-          const projectWrapper = deleteLink.parentNode.parentNode;
-        if (projectWrapper) {
-          projectWrapper.parentNode.removeChild(projectWrapper);
-        } else {
-          console.error('Erreur: projectWrapper est null');
-        }
-          console.error('Erreur lors de la suppression du projet');
-        }
-      };
-      xhr.send('id_artiste=' + artisteId);
+        confirmButton.addEventListener('click', function(event) {
+          event.preventDefault();
+          const xhr = new XMLHttpRequest();
+          xhr.open('POST', 'delete_artiste.php');
+          xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+          xhr.onload = function() {
+            if (xhr.status === 200) {
+              const projectWrapper = deleteLink.closest('.art_wrap');
+              projectWrapper.parentNode.removeChild(projectWrapper);
+              modal.style.display = 'none';
+              showPage(currentPage);
+              updateActiveButtonStates();
+            } else {
+              console.error('Erreur lors de la suppression de l\'artiste');
+            }
+          };
+          xhr.send('id_artiste=' + artisteId);
+        });
+      }
     });
   });
+});
 </script>
