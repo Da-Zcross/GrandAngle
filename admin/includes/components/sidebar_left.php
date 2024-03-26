6a604926668f18b585668077ec1bfb6d53b5b709
mounzor <jb.mounier@yahoo.com>
2024-03-26 10:20:33 +0100

    test

--- admin/includes/components/sidebar_left.php
+++ admin/includes/components/sidebar_left.php
@@ -8,7 +8,7 @@
         <li><a href="./oeuvres.php">Oeuvres</a></li>
         <li><a href="./artistes.php">Artistes</a></li>
         <li><a href="./collaborateurs.php">Collaborateurs</a></li>
-        <li><a href="#">Alertes</a></li>
+        <li><a href="./flashcode.php">Flashcode</a></li>
         <li><a href="#">Paramètres</a></li>
         <li><a href="#">Infos Utile</a></li>
     </ul>
